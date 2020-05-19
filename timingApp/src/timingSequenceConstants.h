beb254729e5e42bca5ce494de83f0c96241d93aa
Eric Norum <wenorum@lbl.gov>
2020-05-19 10:36:20 -0700

    Fix some comments.  Remove unused macro.

--- timingApp/src/timingSequenceConstants.h
+++ timingApp/src/timingSequenceConstants.h
@@ -1,20 +1,20 @@
 #ifndef __TIMINGSEQUENCECONSTANTS_H__
 #define __TIMINGSEQUENCECONSTANTS_H__
 
+// Array capacities
 #define MAX_SEQUENCE_LENGTH   100
 #define EVG_SEQUENCE_CAPACITY 200
 
-// Some ranges for request parameters
+// Ranges for request parameters
 #define MIN_BUCKETS 1
 #define MAX_BUCKETS 328
 #define MIN_BUNCHES 1
 #define MAX_BUNCHES 16
 
-// Delay windows (seconds)
+// Delay window (seconds)
 #define REQUEST_WINDOW 0.5
-#define LOAD_WINDOW 0.2
 
-// Special modes
+// Special mode
 #define SRINJECTION_MODE      40
 
 // Special events
