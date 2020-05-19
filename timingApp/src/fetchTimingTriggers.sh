e00dc3cdf4e9836d725122bfe61483c9183d6406
Eric Norum <wenorum@lbl.gov>
2020-05-19 14:42:55 -0700

    Add mention of other event spreadsheet.

--- timingApp/src/fetchTimingTriggers.sh
+++ timingApp/src/fetchTimingTriggers.sh
@@ -1,1 +1,8 @@
+#!/bin/sh
+
 curl 'https://docs.google.com/spreadsheets/d/1nL-tVF70ZL6u0yFOP9R-MrTJlzp7vyODLzUacy3iOh4/export?exportFormat=csv' >TuningModeTriggers.csv
+
+#
+# Complete list of assigned event codes is at
+# https://docs.google.com/spreadsheets/d/18DQ0N-CdIYjkNm_MtkHIHVrmkWSCliHeTL6w5Q3EvnA/
+#
