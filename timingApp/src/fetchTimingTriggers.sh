6502d038eea52abd8aff62f7fec1eaed81662547
Eric Norum <wenorum@lbl.gov>
2020-11-13 10:16:43 -0800

    Give up on automated download for now.

--- timingApp/src/fetchTimingTriggers.sh
+++ timingApp/src/fetchTimingTriggers.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-curl 'https://docs.google.com/spreadsheets/d/1nL-tVF70ZL6u0yFOP9R-MrTJlzp7vyODLzUacy3iOh4/export?exportFormat=csv' >TuningModeTriggers.csv
+echo 'The curl command seems to no longer work.  You will have to manually export the spreadsheet from the browser and copy it here'
+#curl 'https://docs.google.com/spreadsheets/d/1nL-tVF70ZL6u0yFOP9R-MrTJlzp7vyODLzUacy3iOh4/export/tq?tqx=out:csv' >TuningModeTriggers.csv
 
 #
 # Complete list of assigned event codes is at
