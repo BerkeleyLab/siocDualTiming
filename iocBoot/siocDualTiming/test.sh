7fc8d387f404b0ff122611d6c815a1817e860558
Eric Norum <wenorum@lbl.gov>
2020-11-30 16:03:27 -0800

    Fix up argument handling and make help message consistent.

--- iocBoot/siocDualTiming/test.sh
+++ iocBoot/siocDualTiming/test.sh
@@ -8,16 +8,17 @@ export SEQ_DEBUG="0"
 for i in "$@"
 do
     case "$i" in
-        [0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*.[0-9][0-9]*.)  EVG_ADDRESS="$1" ;;
+        [0-9]*)  EVG_ADDRESS="$1" ;;
         -s) FPGA_SIMM_DISABLE="" ;;
         -t) FPGA_TRACK_DISABLE="" ;;
         -d) SEQ_DEBUG="1" ;;
         -a) export AUTOSAVE_PATH="\$(TOP)/autosave" ;;
-        -*) echo "Usage: $0 [-a] [-d] [-h] [-s]" >&2
+        -*) echo "Usage: $0 [-a] [-d] [-h] [-s] [-t] [EVG_ADDR]" >&2
             echo "       -a -- Use local autosave/restore directory" >&2
             echo "       -d -- Enable injection sequence program diagnostic messages" >&2
             echo "       -h -- Show this help messdage, then exit" >&2
             echo "       -s -- Place EVG records into simulation mode" >&2
+            echo "       -t -- Track old timing system" >&2
             exit 1 ;;
     esac
 done
