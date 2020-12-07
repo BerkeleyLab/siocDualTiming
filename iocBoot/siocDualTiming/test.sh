cc5c953d2962ef0bb43327cfca97f7928b17be77
Eric Norum <wenorum@lbl.gov>
2020-12-07 15:05:33 -0800

    Clean up screens.  Add multiple debug levels.

--- iocBoot/siocDualTiming/test.sh
+++ iocBoot/siocDualTiming/test.sh
@@ -3,22 +3,19 @@
 # Start soft IOC in test mode
 export EVG_ADDRESS="${EVG_ADDRESS=192.168.1.129}"
 export FPGA_SIMM_DISABLE="#"
-export FPGA_TRACK_DISABLE="#"
 export SEQ_DEBUG="0"
 for i in "$@"
 do
     case "$i" in
         [0-9]*)  EVG_ADDRESS="$i" ;;
         -s) FPGA_SIMM_DISABLE="" ;;
-        -t) FPGA_TRACK_DISABLE="" ;;
         -d) SEQ_DEBUG="1" ;;
         -a) export AUTOSAVE_PATH="\$(TOP)/autosave" ;;
-        -*) echo "Usage: $0 [-a] [-d] [-h] [-s] [-t] [EVG_ADDR]" >&2
+        -*) echo "Usage: $0 [-a] [-d] [-h] [-s] [EVG_ADDR]" >&2
             echo "       -a -- Use local autosave/restore directory" >&2
             echo "       -d -- Enable injection sequence program diagnostic messages" >&2
             echo "       -h -- Show this help messdage, then exit" >&2
             echo "       -s -- Place EVG records into simulation mode" >&2
-            echo "       -t -- Track old timing system" >&2
             exit 1 ;;
     esac
 done
--- opi/runEdm.sh
+++ opi/runEdm.sh
@@ -1,13 +1,38 @@
 #!/bin/sh
 
-T=""
-edl="TIM_injectionSequencer.edl"
+T="dual"
+P="testEVG"
+R=":"
+E="TIM_injectionSequencer.edl"
 
-for i in "$@"
+getPath() {
+    for d in . ..
+    do
+        if [ -r "$d/Makefile" -a -r "$d/configure/RELEASE" ]
+        then
+            (
+            cd "$d"
+            SVIFS="$IFS"
+            IFS="=$IFS"
+            set -- `make -pn | grep "^ *$1 *="`
+            IFS="$SVIFS"
+            echo "$2"
+            )
+            break
+        fi
+    done 
+}
+
+ARGS="$@"
+
+EVG=`getPath EVG`
+export EDMDATAFILES=".:$EVG/opi"
+
+for i in $ARGS
 do
     case "$i" in
-    *.edl)  edl="$i" ;;
-    *)      T="$i" ;;
+    *.edl)       E="$i"  ;;
+    *)           P="$i"  R=":" ;;
     esac
 done
 
@@ -16,4 +41,4 @@ case "$T" in
     *)  TARG="T=$T"     ;;
 esac
 
-edm -eolc -x -m $TARG "$edl" &
+edm -eolc -x -m "P=${P},R=${R},${TARG}" "$E" &
