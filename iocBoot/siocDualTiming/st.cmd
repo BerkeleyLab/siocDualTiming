81a6475c0e95857bf3453307e2803903d4678a1e
Eric Norum <wenorum@lbl.gov>
2022-04-21 14:04:35 -0700

    Use simpler environment setup.

--- iocBoot/siocDualTiming/st.cmd
+++ iocBoot/siocDualTiming/st.cmd
@@ -2,9 +2,8 @@
 
 ###############################################################################
 # Set up environment
-epicsEnvSet "DUAL_TIMING_IOC_TEST" "$(DUAL_TIMING_IOC_TEST=test)"
+epicsEnvSet "T" "$(DUAL_TIMING_IOC_TEST=test)"
 epicsEnvSet "EVG_ADDRESS" "$(EVG_ADDRESS=131.243.93.169)"
-epicsEnvSet "T" "$(DUAL_TIMING_IOC_TEST)"
 < envPaths
 epicsEnvSet "IOCSH_PS1" "$(IOC)> "
 
