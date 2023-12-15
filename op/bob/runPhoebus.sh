232da0f57d0aa5e951b5d345e19b6c6479a96fdb
Lucas Russo <lerwys@gmail.com>
2023-12-15 15:54:06 -0800

    op/bob: add phoebus convenience script

---
+++ op/bob/runPhoebus.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+set -eu
+
+prefix="EVG:"
+bob="TIM_injectionSequencer.bob"
+
+SCRIPTPATH="$( cd "$( dirname "${BASH_SOURCE[0]}"  )" && pwd  )"
+
+getPath() {
+    for d in $SCRIPTPATH $SCRIPTPATH/.. $SCRIPTPATH/../..
+    do
+        if [ -r "$d/Makefile" -a -r "$d/configure/RELEASE" ]
+        then
+            (
+            cd "$d"
+            SVIFS="$IFS"
+            IFS="=$IFS"
+            set -- `make -pn | grep "^ *$1 *="`
+            IFS="$SVIFS"
+            echo "$2"
+            )
+            break
+        fi
+    done
+}
+
+for i in "$@"
+do
+    case "$i" in
+    *.bob)  bob="$i" ;;
+    *)      prefix="$i" ;;
+    esac
+done
+
+P=`echo "$prefix" | sed -ne '/\(.\).*/s//\1/p'`
+R=`echo "$prefix" | sed -ne '/.\(.*\)/s//\1/p'`
+EVG=`getPath EVG`
+
+ln -sf $EVG/op/bob/autoconvert/*.bob autoconvert/
+
+#phoebus -resource file:${SCRIPTPATH}/autoconvert/${bob}?"P=${P}&R=${R}"
