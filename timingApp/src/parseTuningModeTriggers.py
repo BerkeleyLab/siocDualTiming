bc9e4e5a3d15d6fcb4c2561d326f0d0505f43c2d
Lucas Russo <lerwys@gmail.com>
2026-06-04 13:53:42 -0700

    timingApp: add support for individual Timestamp columns per mode

--- timingApp/src/parseTuningModeTriggers.py
+++ timingApp/src/parseTuningModeTriggers.py
@@ -4,16 +4,50 @@ import csv
 import sys
 import re
 
+from collections import defaultdict
+
 EventColumn = -1
 OffsetColumn = -1
-ExpressionColumn = -1
 CategoryColumn = -1
 TickOffsetColumn = -1
 
 modeDict = {}
 modeCategoryDict = {}
-duplicateCheckDict = {}
-consistencyCheckDict = {}
+modeFuncTimeDict = {}
+
+duplicateCheckDict = defaultdict(dict)
+consistencyCheckDict = defaultdict(dict)
+
+
+def checkForDuplicates(mode, evt, signature, checkDict, lineno):
+    if mode in checkDict and signature in checkDict[mode]:
+        duplicateEvent = checkDict[mode][signature]
+
+        if evt != duplicateEvent:
+            print(
+                f"Line {lineno} -- Event {evt} is identical to event {duplicateEvent} for mode {mode}"
+            )
+            print(f"signature -- {signature}")
+            return True
+    else:
+        checkDict[mode][signature] = evt
+
+    return False
+
+
+def checkForInconsistency(mode, evt, signature, checkDict, lineno):
+    if mode in checkDict and evt in checkDict[mode]:
+        matchSignature = checkDict[mode][evt]
+        if signature != matchSignature:
+            print(
+                f"Line {lineno} -- Event {evt} has different conditions/actions for mode {mode}"
+            )
+            print(f"signature -- {signature}")
+            return True
+    else:
+        checkDict[mode][evt] = signature
+
+    return False
 
 
 def match(pattern, string):
@@ -47,12 +81,17 @@ with open("TuningModeTriggers.csv") as csvFile:
     inHeader = True
     modeNumberFound = False
     modeNumberDone = False
+    funcTimeModeNumberFound = False
+    funcTimeModeNumberDone = False
     catModeNumberFound = False
+    catModeNumberDone = False
     for line in reader:
         lineno += 1
         if inHeader:
 
-            # Look for descriptive column headers
+            ########################################
+            # Look for Functional "Mode Number"
+            ########################################
             if not modeNumberFound:
                 loc = locationInList(r"Mode[\s]Number", line)
                 if loc is None or loc != 0:
@@ -74,8 +113,6 @@ with open("TuningModeTriggers.csv") as csvFile:
                     EventColumn = c
                 if match(r"Timestamp[\s]Offset", val):
                     OffsetColumn = c
-                if match(r"Functional[\s]Time", val):
-                    ExpressionColumn = c
                 if match(r"Offset[\s]\[Ticks\]", val):
                     TickOffsetColumn = c
                     modeNumberDone = True
@@ -83,15 +120,51 @@ with open("TuningModeTriggers.csv") as csvFile:
             if (
                 EventColumn < 0
                 or OffsetColumn < 0
-                or ExpressionColumn < 0
                 or TickOffsetColumn < 0
                 or len(modeDict) == 0
                 or not modeNumberDone
             ):
-                print(f"Line {lineno} -- Error: Bad heading", file=sys.stderr)
+                print(
+                    f"Line {lineno} -- Error: Bad heading (mode number)",
+                    file=sys.stderr,
+                )
                 sys.exit(1)
 
-            # Look for descriptive column headers
+            ########################################
+            # Look for "Functional Time Mode Number"
+            ########################################
+            if not funcTimeModeNumberFound:
+                loc = locationInList(r"Functional[\s]Time[\s]Mode[\s]Number", line)
+                if loc is None:
+                    continue
+                else:
+                    funcTimeModeNumberFound = True
+
+            for c in range(loc + 1, len(line)):
+                if funcTimeModeNumberDone:
+                    break
+
+                val = line[c]
+                if match(r"[\d]+", val):
+                    if val in modeFuncTimeDict:
+                        print(
+                            f"Column {c + 1} -- Duplicate functional time mode number {val}"
+                        )
+                        sys.exit(1)
+                    modeFuncTimeDict[val] = {"column": c}
+                else:
+                    funcTimeModeNumberDone = True
+
+            if len(modeFuncTimeDict) == 0 or not funcTimeModeNumberDone:
+                print(
+                    f"Line {lineno} -- Error: Bad heading (functional time mode)",
+                    file=sys.stderr,
+                )
+                sys.exit(1)
+
+            ########################################
+            # Look for "Category Mode Number"
+            ########################################
             if not catModeNumberFound:
                 loc = locationInList(r"Category[\s]Mode[\s]Number", line)
                 if loc is None:
@@ -99,16 +172,24 @@ with open("TuningModeTriggers.csv") as csvFile:
                 else:
                     catModeNumberFound = True
 
-            for c in range(loc, len(line)):
+            for c in range(loc + 1, len(line)):
+                if catModeNumberDone:
+                    break
+
                 val = line[c]
                 if match(r"[\d]+", val):
                     if val in modeCategoryDict:
-                        print(f"Column {c + 1} -- Duplicate mode category number {val}")
+                        print(f"Column {c + 1} -- Duplicate category mode number {val}")
                         sys.exit(1)
-                    modeCategoryDict[val] = {"column": c, "categories": []}
+                    modeCategoryDict[val] = {"column": c}
+                else:
+                    catModeNumberDone = True
 
             if len(modeCategoryDict) == 0:
-                print(f"Line {lineno} -- Error: Bad heading", file=sys.stderr)
+                print(
+                    f"Line {lineno} -- Error: Bad heading (category mode)",
+                    file=sys.stderr,
+                )
                 sys.exit(1)
 
             inHeader = False
@@ -124,38 +205,64 @@ with open("TuningModeTriggers.csv") as csvFile:
                         file=sys.stderr,
                     )
                     sys.exit(1)
-                # Canonicalize expression
-                expr = line[ExpressionColumn]
-                if match(r"^[\s]*$", expr) or match(r"^[\s]*Start[\s]*$", expr):
-                    expr = "0"
-                elif match(r"^[\s]*End", expr):
-                    expr = "END_OF_SEQUENCE_TICKS"
-                else:
-                    expr = re.sub(r"InjFieldSync", "injFieldSync", expr, re.IGNORECASE)
-                    expr = re.sub(
-                        r"GunBunchDelay", "gunBunchesDelay", expr, re.IGNORECASE
-                    )
-                    expr = re.sub(r"#[\s]*Bunches", "numBunches", expr, re.IGNORECASE)
-                    expr = re.sub(
-                        r"ExtrFieldSync", "extrFieldSync", expr, re.IGNORECASE
-                    )
-                    expr = re.sub(
-                        r"TargetBucket", "targetBucketDelay", expr, re.IGNORECASE
-                    )
-                    expr = re.sub(r"[\s]+", "", expr)
-                    expr = re.sub(r"\+", " + ", expr)
-                expr = offset(expr, line[TickOffsetColumn], lineno)
-                signature = expr
-                expr = offset(expr, line[OffsetColumn], lineno)
-                tstamp = expr
 
                 # Note modes for which event is active
                 for mode, modeInfo in modeDict.items():
                     col = modeInfo["column"]
                     isActive = line[col]
                     if match(r"^[\s]*x[\s]*$", isActive):
-                        signature = "x" + signature
                         evtList = modeInfo["events"]
+
+                        # Check if we have a functional time column defined for
+                        # this mode
+                        if modeFuncTimeDict[mode] is None:
+                            print(
+                                f"No Functional Time Mode number for mode {mode}",
+                                file=sys.stderr,
+                            )
+                            sys.exit(1)
+
+                        exprCol = modeFuncTimeDict[mode]["column"]
+                        exprStr = line[exprCol]
+
+                        # Canonicalize expression
+                        if match(r"^[\s]*$", exprStr) or match(
+                            r"^[\s]*Start[\s]*$", exprStr
+                        ):
+                            exprStr = "0"
+                        elif match(r"^[\s]*End", exprStr):
+                            exprStr = "END_OF_SEQUENCE_TICKS"
+                        else:
+                            exprStr = re.sub(
+                                r"InjFieldSync", "injFieldSync", exprStr, re.IGNORECASE
+                            )
+                            exprStr = re.sub(
+                                r"GunBunchDelay",
+                                "gunBunchesDelay",
+                                exprStr,
+                                re.IGNORECASE,
+                            )
+                            exprStr = re.sub(
+                                r"#[\s]*Bunches", "numBunches", exprStr, re.IGNORECASE
+                            )
+                            exprStr = re.sub(
+                                r"ExtrFieldSync",
+                                "extrFieldSync",
+                                exprStr,
+                                re.IGNORECASE,
+                            )
+                            exprStr = re.sub(
+                                r"TargetBucket",
+                                "targetBucketDelay",
+                                exprStr,
+                                re.IGNORECASE,
+                            )
+                            exprStr = re.sub(r"[\s]+", "", exprStr)
+                            exprStr = re.sub(r"\+", " + ", exprStr)
+                        exprStr = offset(exprStr, line[TickOffsetColumn], lineno)
+                        exprStr = offset(exprStr, line[OffsetColumn], lineno)
+                        signature = exprStr
+                        tstamp = exprStr
 
                         # Check if we have a category column defined for
                         # this mode
@@ -186,41 +293,37 @@ with open("TuningModeTriggers.csv") as csvFile:
                             sys.exit(1)
 
                         if not any(evtItem.get("number") == evt for evtItem in evtList):
-                            newEvt = {"number": evt,
-                                      "tstamp": tstamp,
-                                      "cat": cat}
+                            newEvt = {"number": evt, "tstamp": tstamp, "cat": cat}
                             evtList.append(newEvt)
+
+                        checkForDuplicates(
+                            mode, evt, signature, duplicateCheckDict, lineno
+                        )
+                        fail = checkForInconsistency(
+                            mode, evt, signature, consistencyCheckDict, lineno
+                        )
+                        if fail:
+                            sys.exit(1)
+
                     elif not match(r"^[\s]*$", isActive):
                         print(
                             f"Line {lineno} -- Error: Bad 'active event' value in column {col + 1}",
                             file=sys.stderr,
                         )
                         sys.exit(1)
-                    else:
-                        signature = " " + signature
-                # Check for duplicates
-                if signature in duplicateCheckDict:
-                    duplicateEvent = duplicateCheckDict[signature]
-                    if evt != duplicateEvent:
-                        print(f"Line {lineno} -- Event %d is identical to event {evt}")
-                else:
-                    duplicateCheckDict[signature] = evt
-                # Check consistency
-                if evt in consistencyCheckDict:
-                    matchSignature = consistencyCheckDict[evt]
-                    if signature != matchSignature:
-                        print(
-                            f"Line {lineno} -- Event {evt} has different conditions/actions"
-                        )
-                        sys.exit(1)
-                else:
-                    consistencyCheckDict[evt] = signature
 
 if inHeader:
     # Header must have been fully processed
     if not modeNumberFound:
         print(
             f'Error: "Mode Number" was not found in header',
+            file=sys.stderr,
+        )
+        sys.exit(1)
+
+    if not funcTimeModeNumberFound:
+        print(
+            f'Error: "Functional Time Mode Number" was not found in header',
             file=sys.stderr,
         )
         sys.exit(1)
@@ -234,7 +337,8 @@ if inHeader:
 
 with open("TimingLookups.h", "w+") as outFile:
     # Emit event list lookup
-    outFile.write("""/*
+    outFile.write(
+        """/*
  * Machine-generated file -- do not edit!
  */
 
@@ -242,7 +346,8 @@ static const unsigned char *
 eventListForMode(int mode)
 {
     switch (mode) {
-""")
+"""
+    )
     for mode, modeInfo in modeDict.items():
         evtList = modeInfo["events"]
         outFile.write(f"    case {mode}: {{ static const unsigned char e[] = {{ ")
@@ -250,19 +355,23 @@ eventListForMode(int mode)
             outFile.write(f"{e['number']}, ")
         print("127 }; return e;}", file=outFile)
 
-    outFile.write("""    default: break;
+    outFile.write(
+        """    default: break;
     }
     return NULL;
 }
-""")
+"""
+    )
 
     # Emit time stamp lookup
-    outFile.write("""
+    outFile.write(
+        """
 static int
 getTimestamp(int mode, int evtCode, int injFieldSync, int extrFieldSync, int numBunches, int gunBunchesDelay, int targetBucketDelay)
 {
     switch (mode) {
-""")
+"""
+    )
     for mode, modeInfo in modeDict.items():
         evtList = modeInfo["events"]
         print(f"    case {mode}:", file=outFile)
@@ -270,24 +379,30 @@ getTimestamp(int mode, int evtCode, int injFieldSync, int extrFieldSync, int num
 
         for e in evtList:
             print(f"        case {e['number']}: return {e['tstamp']};", file=outFile)
-        outFile.write("""        default: return -1;
+        outFile.write(
+            """        default: return -1;
         }
-""")
+"""
+        )
         print("        break;", file=outFile)
-    outFile.write("""    default: break;
+    outFile.write(
+        """    default: break;
 
     }
     return -1;
 }
-""")
+"""
+    )
 
     # Emit category lookup
-    outFile.write("""
+    outFile.write(
+        """
 static int
 getCategory(int mode, int evtCode)
 {
     switch (mode) {
-""")
+"""
+    )
     for mode, modeInfo in modeDict.items():
         evtList = modeInfo["events"]
         print(f"    case {mode}:", file=outFile)
@@ -295,13 +410,17 @@ getCategory(int mode, int evtCode)
 
         for e in evtList:
             print(f"        case {e['number']}: return {e['cat']};", file=outFile)
-        outFile.write("""        default: return -1;
+        outFile.write(
+            """        default: return -1;
         }
-""")
+"""
+        )
         print("        break;", file=outFile)
-    outFile.write("""    default: break;
+    outFile.write(
+        """    default: break;
 
     }
     return -1;
 }
-""")
+"""
+    )
